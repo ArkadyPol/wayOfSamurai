39559e62f72a00759fbbb7b9bf7b1de2379f02e0
Arkady Polhovsky <arkims@inbox.ru>
2021-05-01 01:19:19 +0300

    10 lesson

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,9 @@
 function Header() {
   return (
       <div>
-        <a href="/#">Home</a>
-        <a href="/#">News Feed</a>
-        <a href="/#">Messages</a>
+        <a href="/#">Home</a> -
+        <a href="/#">News Feed</a> -
+        <a href="/#">Messages</a> -
       </div>
   );
 }
