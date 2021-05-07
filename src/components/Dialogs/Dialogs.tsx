7c104731b7dff9309d1182383d6d789e75a5ce27
Arkady Polhovsky <arkims@inbox.ru>
2021-05-07 19:18:00 +0300

    21 lesson

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,8 +2,20 @@ import s from './Dialogs.module.scss';
 
 function Dialogs() {
   return (
-      <div>
-        Dialogs
+      <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+          <div className={s.dialog}>Dima</div>
+          <div className={`${s.dialog} ${s.active}`}>Andrey</div>
+          <div className={s.dialog}>Sveta</div>
+          <div className={s.dialog}>Sasha</div>
+          <div className={s.dialog}>Viktor</div>
+          <div className={s.dialog}>Valera</div>
+        </div>
+        <div className={s.messages}>
+          <div className="messages">Hi</div>
+          <div className="messages">How are you?</div>
+          <div className="messages">What are you doing?</div>
+        </div>
       </div>
   )
 
