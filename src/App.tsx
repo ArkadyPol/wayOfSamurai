520293ebf85aba4260c6dfe4e5289603c7a804fb
Arkady Polhovsky <arkims@inbox.ru>
2021-05-15 21:17:06 +0300

    27 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -42,9 +42,9 @@ function App() {
           <div className="app-wrapper-content">
             <Route path="/dialogs" render={() => <Dialogs dialogs={dialogs} messages={messages}/>}/>
             <Route path="/profile" render={()=> <Profile posts={posts}/>}/>
-            <Route path="/news" component={News}/>
-            <Route path="/music" component={Music}/>
-            <Route path="/settings" component={Settings}/>
+            <Route path="/news" render={News}/>
+            <Route path="/music" render={Music}/>
+            <Route path="/settings" render={Settings}/>
           </div>
         </div>
       </BrowserRouter>
