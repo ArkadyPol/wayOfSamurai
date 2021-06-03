5fe3faf2348032e1453a2eb311537566602fa2f3
Arkady Polhovsky <arkims@inbox.ru>
2021-06-03 12:23:39 +0300

    deploy on github-pages

--- src/render.tsx
+++ src/render.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { RootStateType } from './redux/state'
 
 export const rerenderEntireTree = (state: RootStateType, addPost: (postMessage: string) => void) => {
   ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
       <App state={state} addPost={addPost} />
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
   )
 }
