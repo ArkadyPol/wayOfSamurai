44e5471f5f6f885cb3535521c98944f0efc98759
Arkady Polhovsky <arkims@inbox.ru>
2021-06-06 15:31:19 +0300

    40 lesson

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -12,16 +12,18 @@ type DialogsPropsType = {
 
 function Dialogs(props: DialogsPropsType) {
   const { dialogs, messages, newMessageText } = props.state
+
   const dialogsElements = dialogs.map(d => (
     <DialogItem key={d.id} name={d.name} id={d.id} />
   ))
   const messagesElements = messages.map(m => (
     <Message key={m.id} message={m.message} />
   ))
+
   const sendMessage = () => {
     props.dispatch(sendMessageAC())
   }
-  const onMessageChange = (e: ChangeEvent<HTMLTextAreaElement>)=> {
+  const onMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const message = e.currentTarget.value
     props.dispatch(updateNewMessageTextAC(message))
   }
@@ -29,9 +31,10 @@ function Dialogs(props: DialogsPropsType) {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>{dialogsElements}</div>
-      <div className={s.messages}>{messagesElements}
+      <div className={s.messages}>
+        <div>{messagesElements}</div>
         <div className={s.newMessage}>
-          <textarea value={newMessageText} onChange={onMessageChange}/>
+          <textarea value={newMessageText} onChange={onMessageChange} placeholder='Enter your message' />
         </div>
         <div>
           <button onClick={sendMessage}>Send Message</button>
