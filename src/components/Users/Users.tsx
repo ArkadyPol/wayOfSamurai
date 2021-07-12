8c16cc12abe43745381a7d413c5b4eb22902f1d2
Arkady Polhovsky <arkims@inbox.ru>
2021-07-12 04:13:24 +0300

    53 lesson

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -3,6 +3,7 @@ import User from './User/User'
 import axios from 'axios'
 import { UserType } from '../../redux/users-reducer'
 import s from './Users.module.scss'
+import { Component } from 'react'
 
 type ResponseDataType = {
   items: UserType[]
@@ -10,24 +11,19 @@ type ResponseDataType = {
   error: null | string
 }
 
+class Users extends Component<UsersPropsType> {
 
-function Users(props: UsersPropsType) {
-  const { setUsers } = props
-
-  const getUsers = () => {
+  componentDidMount() {
     axios.get<ResponseDataType>('https://social-network.samuraijs.com/api/1.0/users?count=12')
       .then(response => {
-        setUsers(response.data.items)
+        this.props.setUsers(response.data.items)
       })
   }
 
-
-  const users = props.users.map(u => <User key={u.id} user={u} toggleFollow={props.toggleFollow} />)
-
-  return <>
-    <button onClick={getUsers}>Get Users</button>
-    <div className={s.users}> {users}</div>
-  </>
+  render() {
+    const users = this.props.users.map(u => <User key={u.id} user={u} toggleFollow={this.props.toggleFollow} />)
+    return <div className={s.users}> {users}</div>
+  }
 }
 
 export default Users
