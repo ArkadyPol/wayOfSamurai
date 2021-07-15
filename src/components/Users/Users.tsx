0ff1c6e85039258ad9cf202dbcdc5ab3a15257b3
Arkady Polhovsky <arkims@inbox.ru>
2021-07-15 03:32:22 +0300

    56 lesson

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,61 +1,40 @@
-import { UsersPropsType } from './UsersContainer'
-import User from './User/User'
-import axios from 'axios'
-import { UserType } from '../../redux/users-reducer'
 import s from './Users.module.scss'
-import { Component } from 'react'
+import { UserType } from '../../redux/users-reducer'
+import User from './User/User'
 
-type ResponseDataType = {
-  items: UserType[]
-  totalCount: number
-  error: null | string
+type PropsType = {
+  users: UserType[]
+  toggleFollow: (userId: number) => void
+  totalUsersCount: number
+  pageSize: number
+  currentPage: number
+  onPageChanged: (pageNumber: number) => void
 }
 
-class Users extends Component<UsersPropsType> {
+function Users(props: PropsType) {
+  const { users, toggleFollow, totalUsersCount, pageSize, onPageChanged, currentPage } = props
 
-  componentDidMount() {
-    const { setUsers, currentPage, pageSize, setTotalUsersCount } = this.props
-    axios.get<ResponseDataType>(`https://social-network.samuraijs.com/api/1.0/users?count=${pageSize}&page=${currentPage}`)
-      .then(response => {
-        setUsers(response.data.items)
-        setTotalUsersCount(response.data.totalCount)
-      })
+  const pagesCount = Math.ceil(totalUsersCount / pageSize)
+  const pages = []
+
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i)
   }
-
-  onPageChanged = (pageNumber: number) => {
-    const { setCurrentPage, setUsers, pageSize } = this.props
-    setCurrentPage(pageNumber)
-    axios.get<ResponseDataType>(`https://social-network.samuraijs.com/api/1.0/users?count=${pageSize}&page=${pageNumber}`)
-      .then(response => {
-        setUsers(response.data.items)
-      })
-  }
-
-  render() {
-    const { totalUsersCount, pageSize, users, toggleFollow, currentPage } = this.props
-
-    const pagesCount = Math.ceil(totalUsersCount / pageSize)
-    const pages = []
-
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i)
-    }
-    const pagesElements = pages.map(p => (
-      <span key={p}
-            onClick={() => {
-              this.onPageChanged(p)
-            }}
-            className={`${s.page} ${(p === currentPage) && s.selectedPage}`}>{p}
+  const pagesElements = pages.map(p => (
+    <span key={p}
+          onClick={() => {
+            onPageChanged(p)
+          }}
+          className={`${s.page} ${(p === currentPage) && s.selectedPage}`}>{p}
       </span>
-    ))
-    const usersElements = users.map(u => <User key={u.id} user={u} toggleFollow={toggleFollow} />)
-    return (
-      <>
-        <div> {pagesElements} </div>
-        <div className={s.users}> {usersElements} </div>
-      </>
-    )
-  }
+  ))
+  const usersElements = users.map(u => <User key={u.id} user={u} toggleFollow={toggleFollow} />)
+  return (
+    <>
+      <div> {pagesElements} </div>
+      <div className={s.users}> {usersElements} </div>
+    </>
+  )
 }
 
 export default Users
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,7 +1,50 @@
 import { RootStateType } from '../../redux'
 import { connect, ConnectedProps } from 'react-redux'
-import { toggleFollow, setUsers, UsersPageType, setCurrentPage, setTotalUsersCount } from '../../redux/users-reducer'
+import {
+  toggleFollow,
+  setUsers,
+  UsersPageType,
+  setCurrentPage,
+  setTotalUsersCount,
+  UserType
+} from '../../redux/users-reducer'
+import { Component } from 'react'
+import axios from 'axios'
 import Users from './Users'
+
+type ResponseDataType = {
+  items: UserType[]
+  totalCount: number
+  error: null | string
+}
+
+class UserContainer extends Component<UsersPropsType> {
+
+  componentDidMount() {
+    const { setUsers, currentPage, pageSize, setTotalUsersCount } = this.props
+    axios.get<ResponseDataType>(`https://social-network.samuraijs.com/api/1.0/users?count=${pageSize}&page=${currentPage}`)
+      .then(response => {
+        setUsers(response.data.items)
+        setTotalUsersCount(response.data.totalCount)
+      })
+  }
+
+  onPageChanged = (pageNumber: number) => {
+    const { setCurrentPage, setUsers, pageSize } = this.props
+    setCurrentPage(pageNumber)
+    axios.get<ResponseDataType>(`https://social-network.samuraijs.com/api/1.0/users?count=${pageSize}&page=${pageNumber}`)
+      .then(response => {
+        setUsers(response.data.items)
+      })
+  }
+
+  render() {
+    const { totalUsersCount, pageSize, users, toggleFollow, currentPage } = this.props
+
+    return <Users users={users} toggleFollow={toggleFollow} totalUsersCount={totalUsersCount}
+                  pageSize={pageSize} currentPage={currentPage} onPageChanged={this.onPageChanged} />
+  }
+}
 
 const mapStateToProps = (
   {
@@ -22,4 +65,4 @@ const connector = connect(mapStateToProps, { toggleFollow, setUsers, setCurrentP
 
 export type UsersPropsType = ConnectedProps<typeof connector>
 
-export default connector(Users)
+export default connector(UserContainer)
